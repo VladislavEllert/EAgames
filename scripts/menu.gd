e9b5d520fd20a0cf37f58dbf12f088c5d55a4a0a
Azaт <azatbajmuratov1@gmail.com>
2026-05-08 15:00:18 +0600

    работа над UI

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -114,7 +114,3 @@ func _on_progress_reset() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")

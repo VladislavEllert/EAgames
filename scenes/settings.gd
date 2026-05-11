1e11454c52189e4000826df77b9228a21803a90a
Azaт <azatbajmuratov1@gmail.com>
2026-05-11 11:49:04 +0600

    перенесли сборс прогресса в настройки

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -1,11 +1,14 @@
 extends Panel
 
+@onready var reset_button: Button = $ResetButton
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if reset_button:
+		reset_button.pressed.connect(_on_reset_pressed)
+
+func _on_reset_pressed() -> void:
+	LevelManager.reset_progress()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -114,3 +114,7 @@ func _on_progress_reset() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
